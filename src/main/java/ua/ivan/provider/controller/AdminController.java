c8dade8d60eb3a553357983465f646a82b47d93a
Vanya <memlord1@gmail.com>
2021-08-08 15:46:40 +0300

    add nav

--- src/main/java/ua/ivan/provider/controller/AdminController.java
+++ src/main/java/ua/ivan/provider/controller/AdminController.java
@@ -29,58 +29,54 @@ public class AdminController {
         this.packageService = packageService;
     }
 
-    @GetMapping
-    public String getAllUsers(Model model, Authentication authentication) {
+    @ModelAttribute
+    public void addAttributes(Model model,
+                              Authentication authentication) {
         User user = userDetailsService.getUserByEmail(authentication.getName());
         model.addAttribute("user", user);
         model.addAttribute("listOfUsers", userDetailsService.findAll());
         model.addAttribute("listOfDonates", donateService.findAll());
         model.addAttribute("listOfPackages", packageService.findAll());
+    }
+
+    @GetMapping
+    public String getAllUsers() {
         return "admin";
     }
 
     @PostMapping("/banUser")
-    public String banUser(@RequestParam(value="id") long id, Model model, Authentication authentication) {
+    public String banUser(@RequestParam(value="id") long id) {
         User user = userDetailsService.findById(id);
         user.setStatus(Status.BANNED);
         userDetailsService.saveUser(user);
-        model.addAttribute("user", userDetailsService.getUserByEmail(authentication.getName()));
-        model.addAttribute("listOfUsers", userDetailsService.findAll());
-        model.addAttribute("listOfDonates", donateService.findAll());
         return "admin";
     }
 
     @PostMapping("/unbanUser")
-    public String unbanUser(@RequestParam(value="id") long id, Model model, Authentication authentication) {
+    public String unbanUser(@RequestParam(value="id") long id) {
         User user = userDetailsService.findById(id);
         user.setStatus(Status.ACTIVE);
         userDetailsService.saveUser(user);
-        model.addAttribute("user", userDetailsService.getUserByEmail(authentication.getName()));
-        model.addAttribute("listOfUsers", userDetailsService.findAll());
-        model.addAttribute("listOfDonates", donateService.findAll());
         return "admin";
     }
 
     @PostMapping("/confirm")
-    public String confirm(@RequestParam(value="id_donate") Long donateId, @RequestParam(value="id_user") Long userId, @RequestParam(value="sum") int sum, Model model, Authentication authentication) {
+    public String confirm(@RequestParam(value="id_donate") Long donateId,
+                          @RequestParam(value="id_user") Long userId,
+                          @RequestParam(value="sum") int sum) {
         User user = userDetailsService.findById(userId);
         user.setBalance(user.getBalance() + sum);
         donateService.deleteDonate(donateId);
         userDetailsService.saveUser(user);
-        model.addAttribute("user", userDetailsService.getUserByEmail(authentication.getName()));
-        model.addAttribute("listOfUsers", userDetailsService.findAll());
-        model.addAttribute("listOfDonates", donateService.findAll());
         return "redirect:/admin";
     }
 
     @PostMapping("/unconfirm")
-    public String unconfirm(@RequestParam(value="id_donate") Long donateId, @RequestParam(value="id_user") Long userId, Model model, Authentication authentication) {
+    public String unconfirm(@RequestParam(value="id_donate") Long donateId,
+                            @RequestParam(value="id_user") Long userId) {
         User user = userDetailsService.findById(userId);
         donateService.deleteDonate(donateId);
         userDetailsService.saveUser(user);
-        model.addAttribute("user", userDetailsService.getUserByEmail(authentication.getName()));
-        model.addAttribute("listOfUsers", userDetailsService.findAll());
-        model.addAttribute("listOfDonates", donateService.findAll());
         return "redirect:/admin";
     }
 
